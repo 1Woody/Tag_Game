28d30649e7aecc03fcaea4013181e4c8fc65fff4
CesarTongoliki <Numancia68>
2021-01-05 21:09:02 +0100

    César-V1

--- app/src/main/java/com/example/tag_game/MainActivity.java
+++ app/src/main/java/com/example/tag_game/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.tag_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -15,8 +25,23 @@ public class MainActivity extends AppCompatActivity {
 
     LinearLayout linlay;
     TextView tv;
+    int width,height;
+    ImageView imageview;
+    Bitmap bitmap;
+    Canvas canvas;
+    Bitmap bitmapstar;
+    Paint paint;
+    FrameLayout fralay;
+    Button boto;
 
-    private class ButtonMenu{
+    public void draw(){
+        canvas.drawBitmap(bitmapstar,
+                new Rect(0,0,bitmapstar.getWidth(),bitmapstar.getHeight()),
+                new Rect((int)(0),(int)(0),width,height),paint);
+
+    }
+
+    /*private class ButtonMenu{
         Button bt;
         public ButtonMenu(final String text, Context THIS){
             bt = new Button(THIS);
@@ -32,11 +57,52 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-    }
+    }*/
 
+    @SuppressLint({"ResourceAsColor", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        linlay = new LinearLayout(this);
+        DisplayMetrics metrics=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+        width=metrics.widthPixels;
+        height=metrics.heightPixels;
+        fralay=new FrameLayout(this);
+        fralay.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT));
+        imageview=new ImageView(this);
+        imageview.setLayoutParams(new FrameLayout.LayoutParams(
+                width,height));
+        bitmap= Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
+        imageview.setImageBitmap(bitmap);
+        canvas=new Canvas(bitmap);
+        paint=new Paint();
+        bitmapstar= BitmapFactory.decodeResource(getResources(),R.drawable.tag);
+        draw();
+        boto = new Button(this);
+        boto.setText("JUGAR");
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT,Gravity.CENTER_HORIZONTAL);
+        params.setMargins(0,height/3,0,0);
+        boto.setLayoutParams(params);
+        //boto.setBackgroundColor(R.color.myyellow);
+        boto.setBackgroundColor(Color.YELLOW);
+        boto.setTextColor(Color.BLACK);
+        boto.setTextSize(60);
+        boto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChooseName.class);
+                startActivity(intent);
+            }
+        });
+        fralay.addView(imageview);
+        fralay.addView(boto);
+        setContentView(fralay);
+
+        /*
         String[] gameoptions = {"JUGAR","OPCIONES","SALIR",};
         linlay = new LinearLayout(this);
         tv = new TextView(this);
@@ -48,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         }
         linlay.setOrientation(LinearLayout.VERTICAL);
         linlay.setGravity(Gravity.CENTER_HORIZONTAL);
-        setContentView(linlay);
+        //setContentView(linlay);
+        setContentView(fralay);*/
     }
 }
